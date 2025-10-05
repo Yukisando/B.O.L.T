e56175c27d063ca578b2de99c2fa3fc48848c248
Nathan de Castro <decastronathan@gmail.com>
2025-10-05 09:54:50 +0200

    Fix localization key for B.O.L.T header in multiple languages

--- Locales.lua
+++ Locales.lua
@@ -4,7 +4,7 @@
 local L = {}
 
 -- English (default)
-L["BINDING_HEADER_BOLT"] = "B.O.L.T"
+L["BINDING_HEADER_B_O_L_T"] = "B.O.L.T"
 L["BINDING_NAME_BOLT_TOGGLE_MASTER_VOLUME"] = "Toggle Master Volume"
 L["BINDING_NAME_BOLT_COPY_TARGET_MOUNT"] = "Copy Target Mount"
 
@@ -12,15 +12,15 @@ L["BINDING_NAME_BOLT_COPY_TARGET_MOUNT"] = "Copy Target Mount"
 if GetLocale() == "enUS" or GetLocale() == "enGB" then
     -- Already set above
 elseif GetLocale() == "frFR" then
-    L["BINDING_HEADER_BOLT"] = "B.O.L.T"
+    L["BINDING_HEADER_B_O_L_T"] = "B.O.L.T"
     L["BINDING_NAME_BOLT_TOGGLE_MASTER_VOLUME"] = "Basculer le volume principal"
     L["BINDING_NAME_BOLT_COPY_TARGET_MOUNT"] = "Copier la monture de la cible"
 elseif GetLocale() == "deDE" then
-    L["BINDING_HEADER_BOLT"] = "B.O.L.T"
+    L["BINDING_HEADER_B_O_L_T"] = "B.O.L.T"
     L["BINDING_NAME_BOLT_TOGGLE_MASTER_VOLUME"] = "Hauptlautstärke umschalten"
     L["BINDING_NAME_BOLT_COPY_TARGET_MOUNT"] = "Reittier des Ziels kopieren"
 elseif GetLocale() == "esES" or GetLocale() == "esMX" then
-    L["BINDING_HEADER_BOLT"] = "B.O.L.T"
+    L["BINDING_HEADER_B_O_L_T"] = "B.O.L.T"
     L["BINDING_NAME_BOLT_TOGGLE_MASTER_VOLUME"] = "Alternar volumen maestro"
     L["BINDING_NAME_BOLT_COPY_TARGET_MOUNT"] = "Copiar montura del objetivo"
 end
